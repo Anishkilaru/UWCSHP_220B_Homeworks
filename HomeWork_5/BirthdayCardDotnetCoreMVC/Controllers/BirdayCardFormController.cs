dbc5adbd46edc1076ca171af1482f04a7351a265
AnishKilaru <kilaruanishsai@gmail.com>
2020-06-08 18:18:53 -0700

    HomeWork_5

---
+++ HomeWork_5/BirthdayCardDotnetCoreMVC/Controllers/BirdayCardFormController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BirthdayCardDotnetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdayCardDotnetCoreMVC.Controllers
+{
+    public class BirdayCardFormController : Controller
+    {
+        public IActionResult BirthdayCardForm()
+        {
+            return View();
+        }
+
+        public IActionResult SendBirthdayCard(BirthdayCardModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("BirthdayCardView",model);
+            }
+            return View("BirthdayCardForm", model);
+        }
+        public IActionResult BirthdayCardView(BirthdayCardModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ HomeWork_5/BirthdayCardDotnetCoreMVC/Models/BirthdayCardModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayCardDotnetCoreMVC.Models
+{
+    public class BirthdayCardModel
+    {
+        [Required(ErrorMessage ="Please Enter From")]
+        public string From { get; set; }
+        [Required(ErrorMessage = "Please Enter To")]
+        public string To { get; set; }
+        [Required(ErrorMessage = "Please Enter Message")]
+        public string Message { get; set; }
+    }
+}
